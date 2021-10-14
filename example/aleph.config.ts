243c0eac79c0c1afc5b5a4b3c4cddf31b1922112
calmery <12670155+calmery@users.noreply.github.com>
2021-10-14 13:39:41 +0900

    chore: サンプルコードの aleph.config.ts で `npm install` した @calmery-chan/aleph-plugin-tailwindcss を使用するように変更する

--- example/aleph.config.ts
+++ example/aleph.config.ts
@@ -1,5 +1,5 @@
 import { Config } from "https://deno.land/x/aleph@v0.3.0-beta.19/types.d.ts";
-import tailwindcss from "../mod.ts";
+import tailwindcss from "./node_modules/@calmery-chan/aleph-plugin-tailwindcss/mod.ts";
 
 export default <Config> {
   plugins: [tailwindcss],
